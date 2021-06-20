45354460ba086ae349875f76ef50530059ddc282
X-mei <gemg@mail.uc.edu>
2021-06-20 12:01:22 +0800

    website V1.0 finished

--- js/index.js
+++ js/index.js
@@ -41,3 +41,19 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+function togglePopup1(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglePopup2(){
+  document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglePopup3(){
+  document.getElementById("popup-3").classList.toggle("active");
+}
+
+function togglePopup4(){
+  document.getElementById("popup-4").classList.toggle("active");
+}
